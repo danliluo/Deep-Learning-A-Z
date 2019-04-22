ae561576c8c2e8191aaf1e1b9d22324b1d40ebb3
Danli Luo <me@danliluo.com>
2019-04-22 00:17:45 -0700

    ann

--- Supervised Deep Learning/Artificial Neural Networks (ANN)/ann.py
+++ Supervised Deep Learning/Artificial Neural Networks (ANN)/ann.py
@@ -1,14 +1,5 @@
 # Artificial Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
 # Part 1 - Data Preprocessing
 
 # Importing the libraries
@@ -17,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('dataset/Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
